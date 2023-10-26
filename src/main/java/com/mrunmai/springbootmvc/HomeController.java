d2171eba2f298a83a473d69a1da39259a745a5fb
mrunmai <mrunmaikudale18@gmail.com>
2023-10-26 20:42:18 +0530

    GetMapping annotation to accept only get method, basically used to show the data

--- src/main/java/com/mrunmai/springbootmvc/HomeController.java
+++ src/main/java/com/mrunmai/springbootmvc/HomeController.java
@@ -1,12 +1,16 @@
 package com.mrunmai.springbootmvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mrunmai.springbootmvc.model.Alien;
 
@@ -24,26 +28,35 @@ public class HomeController {
     public String home() {
         return "index";
     }
+    /*
+     * @RequestMapping("add")
+     * public String add(@RequestParam("first") int i, @RequestParam("second") int
+     * j, Model m) {
+     * // Instead of using Request object use RequestParam annotation.
+     * // int i = Integer.parseInt(req.getParameter("first"));
+     * // int j = Integer.parseInt(req.getParameter("second"));
+     * 
+     * // ModelAndView mv = new ModelAndView("result");
+     * // mv.setViewName("result");
+     * int result = i + j;
+     * m.addAttribute("result", result);
+     * 
+     * // HttpSession session = req.getSession();
+     * // session.setAttribute("result", result);
+     * 
+     * return "result";
+     * }
+     */
 
-    @RequestMapping("add")
-    public String add(@RequestParam("first") int i, @RequestParam("second") int j, Model m) {
-        // Instead of using Request object use RequestParam annotation.
-        // int i = Integer.parseInt(req.getParameter("first"));
-        // int j = Integer.parseInt(req.getParameter("second"));
-
-        // ModelAndView mv = new ModelAndView("result");
-        // mv.setViewName("result");
-        int result = i + j;
-        m.addAttribute("result", result);
-
-        // HttpSession session = req.getSession();
-        // session.setAttribute("result", result);
-
-        return "result";
+    @GetMapping("getAliens")
+    public String getAliens(Model m) {
+        List<Alien> aliens = Arrays.asList(new Alien(101, "Mrunmai"), new Alien(102, "Sangram"));
+        m.addAttribute("result",aliens);
+        return "showAliens";
     }
 
-    //Way 1 @RequestMapping(value="addAlien",method=RequestMethod.POST)
-    //Way 2
+    // Way 1 @RequestMapping(value="addAlien",method=RequestMethod.POST)
+    // Way 2
     @PostMapping(value = "addAlien")
     public String addAlien(
             /* @RequestParam("aid") int aid, @RequestParam("aname") String aname, Model m */ @ModelAttribute("a1") Alien a) {
