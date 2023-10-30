188b423fb4e596dbfd08c8ee41c80c5ce8fcb0b5
mrunmai <mrunmaikudale18@gmail.com>
2023-10-31 00:55:26 +0530

    Fetch a single alien detail by passing aid in REST

--- src/main/java/com/mrunmai/springbootmvc/AlienController.java
+++ src/main/java/com/mrunmai/springbootmvc/AlienController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mrunmai.springbootmvc.model.Alien;
@@ -21,4 +22,11 @@ public class AlienController {
         
         return aliens;
     }
+	
+	@GetMapping("alien/{aid}")
+	@ResponseBody
+	public Alien getAlien(@PathVariable("aid") int aid) {
+		Alien a = repo.findById(aid).orElse(new Alien(0,""));
+		return a;
+	}
 }
