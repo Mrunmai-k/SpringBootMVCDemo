af58765d03deec1e4cd0229010c3fe8a7cd73be7
mrunmai <mrunmaikudale18@gmail.com>
2023-10-31 01:00:57 +0530

    instead of writing @ResponseBody to every method change the type of controller. Works only if all the methods are returning json.

--- src/main/java/com/mrunmai/springbootmvc/AlienController.java
+++ src/main/java/com/mrunmai/springbootmvc/AlienController.java
@@ -3,30 +3,31 @@ package com.mrunmai.springbootmvc;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mrunmai.springbootmvc.model.Alien;
 
-@Controller
+@RestController // instead of writing @ResponseBody to every method change the type of
+				// controller. But if some methods are normal methods then go back to mvc
+				// controller.
 public class AlienController {
 	@Autowired
 	AlienRepo repo;
-	
+
 	@GetMapping("aliens")
-	@ResponseBody // otherwise it will consider return statement as jsp page, and not the data.
-    public List<Alien> getAliens() {
-        List<Alien> aliens = repo.findAll();
-        
-        return aliens;
-    }
-	
+//	@ResponseBody // otherwise it will consider return statement as jsp page, and not the data.
+	public List<Alien> getAliens() {
+		List<Alien> aliens = repo.findAll();
+
+		return aliens;
+	}
+
 	@GetMapping("alien/{aid}")
-	@ResponseBody
+//	@ResponseBody
 	public Alien getAlien(@PathVariable("aid") int aid) {
-		Alien a = repo.findById(aid).orElse(new Alien(0,""));
+		Alien a = repo.findById(aid).orElse(new Alien(0, ""));
 		return a;
 	}
 }
