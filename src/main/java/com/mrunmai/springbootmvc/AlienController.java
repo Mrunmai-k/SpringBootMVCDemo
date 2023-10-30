75ee2522a9c4d139cc7abb31efe0e0d2bd4b6846
mrunmai <mrunmaikudale18@gmail.com>
2023-10-31 01:15:06 +0530

    POSTMAPPING of REST to add a alien

--- src/main/java/com/mrunmai/springbootmvc/AlienController.java
+++ src/main/java/com/mrunmai/springbootmvc/AlienController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mrunmai.springbootmvc.model.Alien;
 
 @RestController // instead of writing @ResponseBody to every method change the type of
-				// controller. But if some methods are normal methods then go back to mvc
-				// controller.
+				// controller. Works only if all the methods are returning json.
 public class AlienController {
 	@Autowired
 	AlienRepo repo;
@@ -28,6 +28,12 @@ public class AlienController {
 //	@ResponseBody
 	public Alien getAlien(@PathVariable("aid") int aid) {
 		Alien a = repo.findById(aid).orElse(new Alien(0, ""));
+		return a;
+	}
+	
+	@PostMapping("alien")
+	public Alien addAlien(Alien a) {
+		repo.save(a);
 		return a;
 	}
 }
