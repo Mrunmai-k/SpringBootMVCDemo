57cd20bf5373698e7fca88c45512d1d201f30d8d
mrunmai <mrunmaikudale18@gmail.com>
2023-10-31 20:36:46 +0530

    Use of LOGGER to print the log and create diff file to store the log

--- src/main/java/com/mrunmai/springbootmvc/LoggingAspect.java
+++ src/main/java/com/mrunmai/springbootmvc/LoggingAspect.java
@@ -2,13 +2,17 @@ package com.mrunmai.springbootmvc;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
 	@Before("execution(public * com.mrunmai.springbootmvc.AlienController.getAliens())")
 	public void log() {
-		System.out.println("getAliens method called from aspect...");
+		LOGGER.info("getAliens method called from aspect...");
 	}
 }
